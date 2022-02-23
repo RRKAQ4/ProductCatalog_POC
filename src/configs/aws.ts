d4eea3af4af37d704b4d207eb280ad0167b423ca
Ranjith Kumar R <ranjith@analyticsquad4.com>
2022-02-24 00:35:08 +0530

    Updated aws crd

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
   REGION: "us-east-1",
-  USER_POOL_ID: "us-east-1_RsRm3qkXO",
-  APP_CLIENT_ID: "ua8v1n4n36pmpbme9m79v4684",
+  USER_POOL_ID: "us-east-1_KPndmEZvx",
+  APP_CLIENT_ID: "2qd8epouscmqo874ettlggmud3",
 };
