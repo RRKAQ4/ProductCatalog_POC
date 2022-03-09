21a8b5f5687d279c22a7c4976a6fa80dad4e7a17
Ranjith Kumar R <ranjith@analyticsquad4.com>
2022-03-09 17:53:47 +0530

    Updated cognito id

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
   REGION: "us-east-1",
-  USER_POOL_ID: "us-east-1_7ZvKgN1pN",
-  APP_CLIENT_ID: "4t8f4of5dj3e86jjlcjb1seppb",
+  USER_POOL_ID: "us-east-1_nNhjNNene",
+  APP_CLIENT_ID: "r5kkajn7s58njo9esvkuokl12",
 };
